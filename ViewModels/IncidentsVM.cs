c39484b0381c087c6c35e428af950dac47764210
William Turner <99294754+WTDevs@users.noreply.github.com>
2022-08-02 00:42:45 -0400

    Add files via upload

---
+++ ViewModels/IncidentsVM.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace Proof.Core.ViewModels
+{
+    public class IncidentsVM
+    {
+        public List<Proof.Core.Incident> IncidentsList { get; set; }
+        public IncidentsFilterOptions FilterOptions { get; set; }
+
+        public IncidentsVM()
+        {
+            IncidentsList= new List<Proof.Core.Incident>();
+            FilterOptions = new IncidentsFilterOptions();
+        }
+
+    }
+}
---
+++ ViewModels/SelectedFilterValues.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proof.Core.ViewModels
+{
+    public class SelectedFilterValues
+    {
+        public string Event { get; set; }
+        public string AssignedTo { get; set; }
+        public string FMA { get; set; }
+        public string Status { get; set; }
+        public string CustomerImpact { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+    }
+}
